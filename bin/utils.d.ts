d653a7cbc58a46ba97fa2c2db28a96ce513adec6
Novotný Jan <Jan.Novotny@tescosw.cz>
2019-06-19 11:26:31 +0200

    Repository cleanup

--- bin/utils.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace Utils {
-    function swap(arr: any[], i1: number, i2: number): void;
-}
-declare namespace Utils {
-    function leftPad(s: string, n: number): string;
-    function roundTo(n: number, places: number): number;
-}
-//# sourceMappingURL=utils.d.ts.map
--- cli/things-to-do.js
+++
@@ -1,7 +0,0 @@
-function showTempConversion() {
-    console.log("The boiling point of water in F is " + Convert.cToF(100));
-}
-function showLengthConversion() {
-    var i = 35;
-    console.log(i + " inches formats as " + Convert.formatInches(i) + "\"");
-}
