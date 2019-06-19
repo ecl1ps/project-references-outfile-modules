13dc60175a4ada651d5084c9dcb24a6d6bba372b
Novotný Jan <Jan.Novotny@tescosw.cz>
2019-06-19 11:40:49 +0200

    Added another project to test diamond references

---
+++ tests/length.ts
@@ -0,0 +1,21 @@
+import { leftPad } from "utils";
+
+export const FeetPerMeter = 3.28084;
+
+export function metersToFeet(m: number) {
+    return m / FeetPerMeter;
+}
+
+export function feetToMeters(f: number) {
+    return f * FeetPerMeter;
+}
+
+// Takes a number like 14 and returns a string like `1' 2"`
+export function formatInches(i: number) {
+    if (i < 12) {
+        return `${i}"`;
+    }
+    const ft = Math.floor(i / 12);
+    i = i - ft * 12;
+    return `${ft}' ${leftPad(i.toString(), 2)}`;
+}
